b0706c1c464835b392fa9b242dec486a51f98513
clickhirphae <hpclickomputerhope@gmail.com>
2019-06-13 11:54:45 -0400

    commit message

---
+++ EscapeSequenceII.java
@@ -0,0 +1,13 @@
+//A program displaying the  column values using \t escape sequence
+
+public class EscapeSequenceII {
+    public static void main(String args[]){
+        System.out.println("cost\t\t\tQuantity\tTotal");
+        System.out.println("$1,000.00\t\t4\t\t\t$4,000.00");
+        System.out.println("$ 50.00\t\t\t8\t\t\t$400.00");
+        System.out.println("_________________________________");
+        System.out.println("Total\t\t\t12\t\t\t$4,400.00");
+
+
+    }
+}
